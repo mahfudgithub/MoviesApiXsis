d69e1ce2f33417ab7196e5d07d3094047a4cfcac
mahfudin <mahfudrahman14@gmail.com>
2023-08-12 09:54:29 +0700

    Set up EntityFrameworkCore with postgree SQL

---
+++ Movies21Xsis/DataContext/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies21Xsis.DataContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(GetDefaultConnectionString(), builder =>
+            {
+                builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(3), null);
+            });
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public static string GetDefaultConnectionString()
+        {
+            return Startup.ConnectionString;
+        }
+    }
+}
--- Movies21Xsis/Startup.cs
+++ Movies21Xsis/Startup.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Movies21Xsis.DataContext;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,17 +16,37 @@ namespace Movies21Xsis
 {
     public class Startup
     {
+        public static string ConnectionString { get; private set; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            ConnectionString = Configuration.GetConnectionString("DefaultConnection");
         }
 
         public IConfiguration Configuration { get; }
+        private readonly string _policyName = "CorsPolicy";
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(option =>
+            {
+                option.UseNpgsql(ConnectionString);
+            });
+
+            services.AddCors(option =>
+            {
+                option.AddPolicy(_policyName, builder =>
+                {
+                    builder
+                    .WithOrigins(Configuration["AppClientUrl"])
+                    .WithMethods("PUT", "DELETE", "POST", "GET")
+                    .AllowAnyHeader();
+                });
+            });
+
             services.AddControllers();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +60,7 @@ namespace Movies21Xsis
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseCors(_policyName);
 
             app.UseEndpoints(endpoints =>
             {
